5a3c14aee5625342da2f6785eda749700209dfcf
Monis Khan <i@monis.app>
2023-10-07 21:50:37 -0400

    Prevent rapid reset http2 DOS on API server
    This change fully addresses CVE-2023-44487 and CVE-2023-39325 for
    the API server when the client is unauthenticated.
    
    The changes to util/runtime are required because otherwise a large
    number of requests can get blocked on the time.Sleep calls.
    
    For unauthenticated clients (either via 401 or the anonymous user),
    we simply no longer allow such clients to hold open http2
    connections.  They can use http2, but with the performance of http1
    (with keep-alive disabled).
    
    Since this change has the potential to cause issues, the
    UnauthenticatedHTTP2DOSMitigation feature gate can be disabled to
    remove this protection (it is enabled by default).  For example,
    when the API server is fronted by an L7 load balancer that is set up
    to mitigate http2 attacks, unauthenticated clients could force
    disable connection reuse between the load balancer and the API
    server (many incoming connections could share the same backend
    connection).  An API server that is on a private network may opt to
    disable this protection to prevent performance regressions for
    unauthenticated clients.
    
    For all other clients, we rely on the golang.org/x/net fix in
    https://github.com/golang/net/commit/b225e7ca6dde1ef5a5ae5ce922861bda011cfabd
    That change is not sufficient to adequately protect against a
    motivated client - future changes to Kube and/or golang.org/x/net
    will be explored to address this gap.
    
    The Kube API server now uses a max stream of 100 instead of 250
    (this matches the Go http2 client default).  This lowers the abuse
    limit from 1000 to 400.
    
    Signed-off-by: Monis Khan <mok@microsoft.com>

--- staging/src/k8s.io/apiserver/pkg/features/kube_features.go
+++ staging/src/k8s.io/apiserver/pkg/features/kube_features.go
@@ -178,6 +178,24 @@ const (
 	//
 	// Enables server-side field validation.
 	ServerSideFieldValidation featuregate.Feature = "ServerSideFieldValidation"
+
+	// owner: @enj
+	// beta: v1.29
+	//
+	// Enables http2 DOS mitigations for unauthenticated clients.
+	//
+	// Some known reasons to disable these mitigations:
+	//
+	// An API server that is fronted by an L7 load balancer that is set up
+	// to mitigate http2 attacks may opt to disable this protection to prevent
+	// unauthenticated clients from disabling connection reuse between the load
+	// balancer and the API server (many incoming connections could share the
+	// same backend connection).
+	//
+	// An API server that is on a private network may opt to disable this
+	// protection to prevent performance regressions for unauthenticated
+	// clients.
+	UnauthenticatedHTTP2DOSMitigation featuregate.Feature = "UnauthenticatedHTTP2DOSMitigation"
 )
 
 func init() {
@@ -195,6 +213,7 @@ var defaultKubernetesFeatureGates = map[featuregate.Feature]featuregate.FeatureS
 	RemainingItemCount:                  {Default: true, PreRelease: featuregate.Beta},
 	ServerSideApply:                     {Default: true, PreRelease: featuregate.GA},
 	StorageVersionHash:                  {Default: true, PreRelease: featuregate.Beta},
+	UnauthenticatedHTTP2DOSMitigation:   {Default: true, PreRelease: featuregate.Beta},
 	StorageVersionAPI:                   {Default: false, PreRelease: featuregate.Alpha},
 	WatchBookmark:                       {Default: true, PreRelease: featuregate.GA, LockToDefault: true},
 	APIPriorityAndFairness:              {Default: true, PreRelease: featuregate.Beta},
